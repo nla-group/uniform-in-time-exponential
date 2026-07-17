9e800fb74d726504bf5c2e95bc9d2dfa5848e1b0
ShuaiShao-1203 <shuai.shao-7@postgrad.manchester.ac.uk>
2026-07-17 17:17:10 +0100

    Add files via upload

---
+++ myaddpath.m
@@ -0,0 +1,12 @@
+%% User-Speficied Path
+rkfit_path = 'D:\Matlab\rktoolbox';
+% IMPORTANT: Replace this path with the installation directory of your
+% RKFIT Toolbox.
+
+mp_path = 'D:\Matlab\Multiprecision Computing Toolbox';
+% IMPORTANT: Replace this path with the installation directory of your
+% Advanpix Multiprecision Computing Toolbox.
+
+%% Add paths.
+addpath(rkfit_path)
+addpath(mp_path)
